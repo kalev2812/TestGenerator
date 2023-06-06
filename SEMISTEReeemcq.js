f0d6add8a64c17f65cedb75902df228900e01b01
kalev2812 <96040079+kalev2812@users.noreply.github.com>
2023-06-06 09:53:27 +0530

    Add files via upload

---
+++ SEMISTEReeemcq.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "Among the given engineers, who is known as the first female environmental engineer? ",
+    choice1: "Ellen Swallow Richards",
+    choice2: "Miss Ellen Henrietta ",
+    choice3: "Catherine Anselm Gleason",
+    choice4: " Elsie Eaves",
+    answer: 1
+},
+{
+    question: "Which of the following is/are types of pollution that affect the environment?",
+    choice1: "Air pollution",
+    choice2: "Water pollution",
+    choice3: "Land pollution",
+    choice4: "All of the above",
+    answer: 4
+},
+{
+    question: "Why carbon dioxide is called a greenhouse gas?  ",
+    choice1: "Because they absorb heat",
+    choice2: "Because they absorb moisture",
+    choice3: "Because they absorb oxygen",
+    choice4: "Because they absorb hydrogen",
+    answer: 1
+},
+{
+    question: "Which of the following is a renewable source of energy?",
+    choice1: "Ocean currents",
+    choice2: "Solar energy",
+    choice3: "Biomass  ",
+    choice4: "All of the above",
+    answer: 4
+},
+{
+    question: "Which is the first zone of purification in a sand bed?",
+    choice1: "Heterotrophic zone",
+    choice2: "Schmutzdecke zone",
+    choice3: "Electrolytic zone",
+    choice4: " Autotrophic zone",
+    answer: 2
+},
+{
+    question: "Which of the following compounds was earlier produced for the utility of transformers? ",
+    choice1: "PCB  ",
+    choice2: "TCDD",
+    choice3: "PCDF",
+    choice4: "PDD",
+    answer: 1
+},
+{
+    question: "Which of the following is NOT a primary pollutant? ",
+    choice1: "Oxygen ",
+    choice2: "Ground-level ozone",
+    choice3: " Carbon monoxide",
+    choice4: "Carbon dioxide",
+    answer: 1
+}, {
+    question: "Which of the following gas is released when alum is added to water?",
+    choice1: "Ca (OH)3 ",
+    choice2: "CO2",
+    choice3: "Al (OH)3 ",
+    choice4: "CaSO4",
+    answer: 2
+},
+{
+    question: "Which of the following is biodegradable waste?",
+    choice1: "Paper",
+    choice2: "Food waste",
+    choice3: "Polythene bags",
+    choice4: "Synthetic fiber",
+    answer: 2
+},
+{
+    question: "Which of the following type of pollution is Cultural eutrophication? ",
+    choice1: "Noise pollution",
+    choice2: "Thermal pollution",
+    choice3: "Soil pollution",
+    choice4: "Water pollution",
+    answer: 4
+}];
